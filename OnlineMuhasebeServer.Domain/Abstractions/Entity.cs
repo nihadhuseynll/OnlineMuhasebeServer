23002d3d91356f5130a724c0f0f94797006e2102
Nikoli <huseynlinihad4400@gmail.com>
2024-07-14 17:54:00 +0400

    Add project files.

---
+++ OnlineMuhasebeServer.Domain/Abstractions/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineMuhasebeServer.Domain.Abstractions
+{
+	public abstract class Entity
+	{
+		public string Id { get; set; }
+		public DateTime CretedDate { get; set; }
+		public DateTime UpdateDate { get; set; }
+	}
+}
---
+++ OnlineMuhasebeServer.Domain/AppEntities/UserAndCompanyRelationShip.cs
@@ -0,0 +1,17 @@
+﻿using OnlineMuhasebeServer.Domain.Abstractions;
+using OnlineMuhasebeServer.Domain.AppEntities.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineMuhasebeServer.Domain.AppEntities
+{
+	public sealed class UserAndCompanyRelationShip : Entity
+	{
+		[ForeignKey("AppUser")]
+		public string AppUserId { get; set; }
+		public AppUser AppUser { get; set; }
+
+		[ForeignKey("Company")]
+		public string CompanyId { get; set; }
+		public Company Company { get; set; }
+	}
+}
